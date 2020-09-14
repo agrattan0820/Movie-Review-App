5ad4caea648b45770dbd1fad23d1ce3bbd7bca5c
Alexander Grattan <alex@grattan.me>
2020-09-14 10:36:18 -0400

    API Info

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const APP_ID = "d8a584d9-bd2a-4731-bee0-de61d97049ba";
+  const API_KEY = "pkHjVPMSv0I48ATgtjYQBaDyGJGIszNk";
+
+  const exampleCall =
+    "https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=godfather&api-key=yourkey";
+
   return (
     <div className="App">
       <header className="App-header">
