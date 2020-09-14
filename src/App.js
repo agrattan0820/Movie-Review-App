25e129b8c474e8fd65b3ada452a1c3bed085362c
Alexander Grattan <alex@grattan.me>
2020-09-14 13:55:02 -0400

    Successfully grab image and title

--- src/App.js
+++ src/App.js
@@ -1,30 +1,36 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useEffect, useState } from "react";
+import Movie from "./Movie";
 import "./App.css";
 
 function App() {
-  const APP_ID = "d8a584d9-bd2a-4731-bee0-de61d97049ba";
   const API_KEY = "pkHjVPMSv0I48ATgtjYQBaDyGJGIszNk";
 
-  const exampleCall =
-    "https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=godfather&api-key=yourkey";
+  const [reviews, setReviews] = useState([]);
+
+  useEffect(() => {
+    getReviews();
+  }, []);
+
+  const getReviews = async () => {
+    const response = await fetch(
+      `https://api.nytimes.com/svc/movies/v2/reviews/picks.json?api-key=${API_KEY}`
+    );
+    const reviewData = await response.json();
+    setReviews(reviewData.results);
+    console.log(reviewData.results);
+  };
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="search-form">
+        <input className="search-bar" type="text" />
+        <button className="search-button" type="submit">
+          Search
+        </button>
+      </form>
+      {reviews.map((review) => (
+        <Movie title={review.display_title} image={review.multimedia.src} />
+      ))}
     </div>
   );
 }
--- src/Movie.js
+++ src/Movie.js
@@ -1,24 +1,12 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
-function Movie() {
+function Movie({ title, image }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>{title}</h1>
+      <p>Description</p>
+      <img src={image} alt="" />
     </div>
   );
 }
