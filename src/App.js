7ef939531679993fd0f82d2c685f2d006d3393fa
Alexander Grattan <alex@grattan.me>
2020-09-14 22:53:04 -0400

    Start of pagination

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import "./sass/App.scss";
 
 function App() {
   const API_KEY = "pkHjVPMSv0I48ATgtjYQBaDyGJGIszNk";
+  const offsetNum = 0;
 
   const [reviews, setReviews] = useState([]);
+  const [morePages, setMorePages] = useState();
   const [loading, setLoading] = useState(false);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [postsPerPage, setPostsPerPage] = useState(15);
 
   useEffect(() => {
     getReviews();
@@ -17,12 +17,13 @@ function App() {
   const getReviews = async () => {
     setLoading(true);
     const response = await fetch(
-      `https://api.nytimes.com/svc/movies/v2/reviews/picks.json?api-key=${API_KEY}`
+      `https://api.nytimes.com/svc/movies/v2/reviews/picks.json?api-key=${API_KEY}&offset=${offsetNum}`
     );
     const reviewData = await response.json();
     setReviews(reviewData.results);
+    setMorePages(reviewData.has_more);
     setLoading(false);
-    console.log(reviewData.results);
+    console.log(reviewData);
   };
 
   return (
@@ -30,17 +31,25 @@ function App() {
       <header>
         <h1>NYT Critic's Picks</h1>
       </header>
-      <div className="movie-list">
-        {reviews.map((review) => (
-          <Movie
-            key={review.display_title}
-            title={review.display_title}
-            description={review.summary_short}
-            image={review.multimedia.src}
-          />
-        ))}
-      </div>
-      <footer></footer>
+      {loading && <h2>Loading...</h2>}
+      {!loading && (
+        <div className="movie-list">
+          {reviews.map((review) => (
+            <Movie
+              key={review.display_title}
+              title={review.display_title}
+              description={review.summary_short}
+              image={review.multimedia.src}
+            />
+          ))}
+        </div>
+      )}
+      <footer>
+        <nav>
+          {offsetNum === 0 && <button>Previous Page</button>}
+          {morePages && <button> Next Page</button>}
+        </nav>
+      </footer>
     </div>
   );
 }
